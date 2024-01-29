b262d20bea5c6d49777277acc88b8bd6534482c1
Wagdy <studybondok@gmail.com>
2024-01-30 01:07:19 +0200

    function intterupt

--- StartupCode-Stm32f401.c
+++ StartupCode-Stm32f401.c
@@ -1,7 +1,42 @@
 /******************** Startup Code *********************/
 
+extern int _estack;
+void Reset_Handler(void);
+void NMI_Handler(void);
+void HardFault_Handler(void);
 // 1. setting the interrupt vector table on [.isr_vector] section
 
+typedef void (*const PF_Handler)(void);   //make a pointer to function with data type
+//then I want to make the vector table array below be in the ((isr vctr)) in the flash 
+__attribute__((section(".isr_vector"),used)) //used keyword is used to guide all the content to be in the flash don't matter it's used or not
+
+
+//the PF_Handler is datatype now for a pointer point to function with it's address so the if we need to make an interrupt we use the pointer that holds the address of the function to be excuted
+
+
+//making the vector table and filling it with the data from the datasheet
+PF_Handler Vector_table[]={ 
+    (PF_Handler)&_estack,
+    &Reset_Handler,
+    &NMI_Handler,          
+    &HardFault_Handler,    
+
+              };
+
+
+//impelementation for the functions of the inttrupt table
+
+void Reset_Handler(void){
+
+}
+void NMI_Handler(void){
+
+}
+
+void HardFault_Handler(void){
+
+    
+}
 //2. intialize the stack pointer
 
 //3.copy [.data] section from Flash to RAM
