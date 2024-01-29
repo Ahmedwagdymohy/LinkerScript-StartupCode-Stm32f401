537645b64e7a7c8836da7c324a7f76a5386bd411
Wagdy <studybondok@gmail.com>
2024-01-29 20:24:29 +0200

    stm32f401cb.pdf: stm3 StartupCode

---
+++ StartupCode-Stm32f401.c
@@ -0,0 +1,13 @@
+/******************* Startup Code *********************/
+
+// 1. setting the interrupt vector table on [.isr_vector] section
+
+//2. intialize the stack pointer
+
+//3.copy [.data] section from Flash to RAM
+
+//4. Reverse[.bss] section on Ram and initialize with zero
+
+//5. call main() function
+
+
