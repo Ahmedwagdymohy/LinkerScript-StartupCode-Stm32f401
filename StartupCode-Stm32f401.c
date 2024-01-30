3492c1615654727666042709456ea7a25c33c51f
Wagdy <studybondok@gmail.com>
2024-01-30 22:26:57 +0200

    implementing

--- StartupCode-Stm32f401.c
+++ StartupCode-Stm32f401.c
@@ -1,6 +1,12 @@
-/******************** Startup Code *********************/
-
+/******************** Startup Code to be in .init *********************/
+//2. intialize the stack pointer
 extern int _estack;
+extern int _sdata_FLASH;/*here we extern the varialbles of the addresses of the data section of the RAM*/
+extern int _sdata;/*start and end (e & s) and the start of the data of flash*/
+extern int _edata;/*so we can move the with startup code from Flash to ram with ((*sdata_Flash++ = *sdata++)) so
+                    we loop on the size of the .data section we have till we transfer all the data*/
+
+
 void Reset_Handler(void);
 void NMI_Handler(void);
 void HardFault_Handler(void);
@@ -14,6 +20,20 @@ void Systick_Handler(void);
 void WWDG_Handler(void);
 void PVD_Handler(Void);
 void TAMP_STAMP_Handler(Void);
+
+//********************BUT if we forgot an impelemntation for any function the program will make error
+/***** to solve this we make an attribute functionwith weak argument to tell the program if the the funtion
+ * not implemented use the default function with no error , and this default function we define! 
+*/
+
+//for example we will make this to the header(prototype) of the function
+void __attribute__ ((weak,alias ("default_Handler")))    EXTI0_Handler(void);
+//we here tell the program if the EXTI0_Handler isn't defined"hasn't implemented" we will use the default handler
+
+
+
+
+
 
 
 // 1. setting the interrupt vector table on [.isr_vector] section
@@ -60,8 +80,24 @@ PF_Handler Vector_table[]={
 
 
 //impelementation for the functions of the inttrupt table
+void __attribute__ ((section(".init"),used)) default_Handler(void){
+    while (1)
+    {
+        /* code */
+    }
+    
+}
 
-void Reset_Handler(void){
+//the real usage of the startup code goes here ,where the startup code start when we Reset or start from zer0!
+
+void __attribute__((section(".isr_vector"),used)) Reset_Handler(void){
+
+//3.copy [.data] section from Flash to RAM
+
+//4. Reverse[.bss] section on Ram and initialize with zero
+
+//5. call main() function
+
 
 }
 void NMI_Handler(void){
@@ -89,7 +125,7 @@ void UsageFault_Handler(void){
 }
 void SVCall_Handler(void){
 
-    
+
 }
 void DebugMonitor_Handler(void){
 
@@ -119,12 +155,8 @@ void TAMP_STAMP_Handler(Void){
 
 
 
-//2. intialize the stack pointer
 
-//3.copy [.data] section from Flash to RAM
 
-//4. Reverse[.bss] section on Ram and initialize with zero
 
-//5. call main() function
 
 
